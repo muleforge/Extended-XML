cfdd2a156dc13e9e65b73fe9cae7196f1cac2707
ross <ross@85650327-e542-0410-ac1c-c6c080e79cab>
2008-05-17 09:50:15 +0000

    fix compile error.  Method not overridden

--- castor/src/main/java/org/mule/module/xml/castor/config/CastorNamespaceHandler.java
+++ castor/src/main/java/org/mule/module/xml/castor/config/CastorNamespaceHandler.java
@@ -5,11 +5,11 @@ import org.mule.config.spring.parsers.specific.TransformerDefinitionParser;
 import org.mule.transformers.xml.castor.CastorObjectToXml;
 import org.mule.transformers.xml.castor.CastorXmlToObject;
 
-public class CastorNamespaceHandler extends AbstractMuleNamespaceHandler {
-
-	@Override
-	public void init() {
-		registerBeanDefinitionParser("object-to-xml",new TransformerDefinitionParser(CastorObjectToXml.class));
-		registerBeanDefinitionParser("xml-to-object",new TransformerDefinitionParser(CastorXmlToObject.class));
-	}
+public class CastorNamespaceHandler extends AbstractMuleNamespaceHandler
+{
+    public void init()
+    {
+        registerBeanDefinitionParser("object-to-xml", new TransformerDefinitionParser(CastorObjectToXml.class));
+        registerBeanDefinitionParser("xml-to-object", new TransformerDefinitionParser(CastorXmlToObject.class));
+    }
 }
